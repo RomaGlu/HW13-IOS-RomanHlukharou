270121f350ce94b36b173e96892d2001f80ff0c3
RomaGlu <roma.glu.69@gmail.com>
2023-06-30 16:41:32 +0300

    add comments, folders

---
+++ HW13-IOS-RomanHlukharou/Sources/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  HW13-IOS-RomanHlukharou
+//
+//  Created by Роман Глухарев on 30/06/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ HW13-IOS-RomanHlukharou/Sources/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  HW13-IOS-RomanHlukharou
+//
+//  Created by Роман Глухарев on 30/06/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
